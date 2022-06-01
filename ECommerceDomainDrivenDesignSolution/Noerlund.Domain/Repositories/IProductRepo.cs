a95d68b57bb3c3442eff2e7e363e7d0c64371273
Kristoffer Nørlund Kruse <kris69j1@edu.ucl.dk>
2022-06-01 16:01:27 +0200

    repositorie for product in domain layer

---
+++ ECommerceDomainDrivenDesignSolution/Noerlund.Domain/Repositories/IProductRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Noerlund.Domain.Models;
+
+namespace Noerlund.Domain.Repositories
+{
+    public interface IProductRepo
+    {
+        Task CreateProductAsync(Product p);
+        Task DeleteProductAsync(Guid id);
+        Task UpdateProductAsync(Product p);
+        Product GetProductByGuidId(Guid id);
+        List<Product> getAllProductsByCategory(string category);
+    }
+}
